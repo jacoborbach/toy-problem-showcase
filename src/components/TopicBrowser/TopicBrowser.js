addd2fc38486c6245aa7d37e07da10f6d7555fb3
jacoborbach <jacoborbach96@gmail.com>
2020-12-15 16:36:30 -0500

    done with problem 1

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TopicBrowser extends Component {
+
+    render() {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
+
+//have to export
+export default TopicBrowser;
