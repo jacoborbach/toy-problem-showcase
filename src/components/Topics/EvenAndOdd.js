6cf430afb9b0b5e5247c4848af248f29120147b2
jacoborbach <jacoborbach96@gmail.com>
2020-12-16 10:34:06 -0500

    done with step 3

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class EvenandOdd extends Component {
+    constructor() {
+        super();
+        // Must add state, this is how react identifies values for the class!
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+    handleChange(event) {
+        this.setState({
+            userInput: event
+        })
+    }
+    assignEvenAndOdds(userInput) {
+        let arr = userInput.split(',');
+        let evens = [];
+        let odds = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 === 0) {
+                evens.push(parseInt(arr[i], 10));
+            } else {
+                odds.push(parseInt(arr[i], 10))
+            }
+        }
+        this.setState({ evenArray: evens, oddArray: odds })
+    }
+
+    // arr.map(e => e % 2 === 0 ? this.state.evenArray.push(e) : this.state.oddArray.push(e))
 
     render() {
         return (
-            <p>Even and Odd</p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                {/* This part onChange is probably wrong*/}
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} />
+                <button className="confirmationButton" onClick={() => this.assignEvenAndOdds(this.state.userInput)} >Split</button>
+                <span className="resultsBox">Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className="resultsBox">Odds: {JSON.stringify(this.state.oddArray)}</span>
+                {/* {this.assignEvenAndOdds /} */}
+            </div>
         )
     }
 }
